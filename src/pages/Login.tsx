f174d629cd3f541c8079cc03ab4d3f344df740f6
Seb <westerholm.sebastian@gmail.com>
2025-09-17 00:36:58 +0200

    WIP: Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,85 @@
+// Uncomment everything when API is in place
+
+import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+import "../styles/login.css";
+
 function Login() {
-  return <div> Login page</div>;
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+  // const [error, setError] = useState("");
+  // const navigate = useNavigate();
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCredentials((prev) => ({ ...prev, [name]: value }));
+  };
+
+  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   setError("");
+
+  //   try {
+  //     const response = await fetch("http://localhost:5000/api/auth/login", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify(credentials),
+  //     });
+
+  //     const data = await response.json();
+
+  //     if (response.ok) {
+  //       localStorage.setItem("token", data.token);
+  //       navigate("/dashboard");
+  //     } else {
+  //       setError(data.message || "Login failed");
+  //     }
+  //   } catch (err) {
+  //     setError("Server error");
+  //   }
+  // };
+
+  /*
+    ----------------------------
+    Remove when API is in place
+    ----------------------------
+  */
+  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(credentials);
+    console.log("Logging in");
+  };
+  /*
+    ----------------------------
+    Remove when API is in place
+    ----------------------------
+  */
+
+  return (
+    <div className="loginContainer">
+      {/* <form className="loginForm" onSubmit={handleSubmit}> */}
+      <form className="loginForm" onSubmit={handleLogin}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={credentials.username}
+          onChange={handleInput}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={credentials.password}
+          onChange={handleInput}
+        />
+        <button type="submit">Login</button>
+      </form>
+      {/* {error && <p className="error">{error}</p>} */}
+    </div>
+  );
 }
 
 export default Login;
