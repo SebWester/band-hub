5f9347ac8e744614543d2b73ed4dca5b3daf857c
Seb <westerholm.sebastian@gmail.com>
2025-09-25 23:07:04 +0200

    fix: remove form from role component. wip: handle signup function

--- src/components/Role.tsx
+++ src/components/Role.tsx
@@ -8,40 +8,38 @@ interface RoleProps {
 function Role({ role, onRoleChange }: RoleProps) {
   return (
     <div className="roleDiv">
-      <form>
-        <label className="roleOption">
-          <input
-            type="radio"
-            name="role"
-            value="Artist"
-            checked={role === "Artist"}
-            onChange={(e) => onRoleChange(e.target.value)}
-          />
-          Artist
-        </label>
+      <label className="roleOption">
+        <input
+          type="radio"
+          name="role"
+          value="Artist"
+          checked={role === "Artist"}
+          onChange={(e) => onRoleChange(e.target.value)}
+        />
+        Artist
+      </label>
 
-        <label className="roleOption">
-          <input
-            type="radio"
-            name="role"
-            value="Band"
-            checked={role === "Band"}
-            onChange={(e) => onRoleChange(e.target.value)}
-          />
-          Band
-        </label>
+      <label className="roleOption">
+        <input
+          type="radio"
+          name="role"
+          value="Band"
+          checked={role === "Band"}
+          onChange={(e) => onRoleChange(e.target.value)}
+        />
+        Band
+      </label>
 
-        <label className="roleOption">
-          <input
-            type="radio"
-            name="role"
-            value="Musician"
-            checked={role === "Musician"}
-            onChange={(e) => onRoleChange(e.target.value)}
-          />
-          Musician
-        </label>
-      </form>
+      <label className="roleOption">
+        <input
+          type="radio"
+          name="role"
+          value="Musician"
+          checked={role === "Musician"}
+          onChange={(e) => onRoleChange(e.target.value)}
+        />
+        Musician
+      </label>
     </div>
   );
 }
